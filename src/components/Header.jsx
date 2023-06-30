95e5213a605f0e8e22ec1078a008edc1ae6c84cb
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-06-30 04:13:42 -0700

    Adds current date to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 export default function Header() {
+  const date = new Date()
+  const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October","November", "December"];
+
   return (
-  <header>
-    <h1>React App</h1>
-  </header>
+    <header>
+      <h1>Top Steam Deals for {month[date.getMonth()] + "/" + date.getDate() + "/" + date.getFullYear()}</h1>
+    </header>
   )
 }
