6b4fa19951e3f60aeaaf13dd1ec8d4310cdb27d3
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-06-30 22:15:53 -0700

    Adds additional info to card layout as well as some styling

--- src/components/DealCard.jsx
+++ src/components/DealCard.jsx
@@ -1,19 +1,44 @@
-import { Card } from "semantic-ui-react";
+import { Button, Card } from "semantic-ui-react";
+import "../styles/DealCard.css"
+
 export default function DealCard({deals}) {
   if (deals === null) {
     return (<Card>Loading ... </Card>)
   } else {
     return (deals.map(deal => {
         return (
-            <Card key={deal.gameID} className="ui aligned">
+            <Card key={deal.gameID} className="ui center aligned">
               <Card.Content>
-                {<img src={deal.thumb} alt={"Thumbnail of" + deal.title}/>}
-                <Card.Header>
-                  {deal.title}
-                </Card.Header>
-                &nbsp;
-                Metacritic Score: {<a href={"https://metacritic.com" + deal.metacriticLink}>{deal.metacriticScore}</a>}
-                &nbsp;
+                {<img 
+                    src={deal.thumb} 
+                    alt={deal.title + " Thumbnail"} 
+                    className="ui image"
+                  />
+                  }                   
+                  <Card.Header id="card-header" className="ui header">
+                    {deal.title}
+                  </Card.Header>
+                <p>
+                  Metacritic Score: {
+                    <a href={"https://metacritic.com" + deal.metacriticLink}>
+                      {deal.metacriticScore}%
+                    </a>
+                    }
+                </p>
+                <p>
+                  Steam Rating: {
+                  <a href={"https://steamcommunity.com/app/" + deal.steamAppID + "/reviews/?browsefilter=toprated"}>
+                    {deal.steamRatingPercent}%
+                  </a>}  
+                </p>
+                <p>
+                  {deal.steamRatingText}
+                </p>
+                <p id="sale-price-text">
+                  <span id="normal-price-text">${deal.normalPrice}</span>
+                  &nbsp;| <strong>${deal.salePrice}</strong>
+                </p>
+                <Button>BUY</Button>              
               </Card.Content>
             </Card>
         )
