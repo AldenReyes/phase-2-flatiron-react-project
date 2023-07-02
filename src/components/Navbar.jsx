1efe5db7f48c0f25602f7d75f228877609e95229
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-07-02 05:28:00 -0700

    Adds initial functionality to search element

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,36 @@
-import { NavLink } from "react-router-dom"
-import { Menu } from "semantic-ui-react"
+import { useState, useRef } from "react";
+import { NavLink } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
 
 export default function Navbar() {
+  const [search, setSearch] = useState("");
+  const searchRef = useRef(null);
+  function handleSearchChange(e) {
+    setSearch(e.target.value);
+  }
+  function handleSearchSubmit(e) {
+    if (e.type === "click" || e.key === "Enter") {
+      console.log(searchRef.current.value);
+    }
+  }
   return (
     <Menu className="ui five item menu">
-      <Menu.Item as={NavLink} to="/" name="Home"/>
-      <Menu.Item as={NavLink} to="saved-deals" name="Saved Deals"/>
-      <Menu.Item as={NavLink} to="About" name="About"/>
-          <div className="menu ui icon input">
-            <input type="text" placeholder="Search..."/>
-            <i className="search link icon" />
-          </div>
+      <Menu.Item as={NavLink} to="/" name="Home" />
+      <Menu.Item as={NavLink} to="saved-deals" name="Saved Deals" />
+      <Menu.Item as={NavLink} to="About" name="About" />
+      <div className="right menu">
+        <div className="ui aligned right icon input ">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={search}
+            ref={searchRef}
+            onChange={handleSearchChange}
+            onKeyDown={handleSearchSubmit}
+          />
+          <i className="search link icon" onClick={handleSearchSubmit} />
+        </div>
+      </div>
     </Menu>
-  )
+  );
 }
