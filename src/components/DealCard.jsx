5a0dd8bbd30d797473494c2a62a5ee6e604f819b
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-07-01 17:24:37 -0700

    adds new tab indicator, formats w/ prettier

--- src/components/DealCard.jsx
+++ src/components/DealCard.jsx
@@ -1,53 +1,67 @@
-import { Button, Card } from "semantic-ui-react";
-import "../styles/DealCard.css"
+import { Card, Loader } from "semantic-ui-react";
+import "../styles/DealCard.css";
 
-export default function DealCard({deals}) {
+export default function DealCard({ deals }) {
   if (deals === null) {
-    return (<Card>Loading ... </Card>)
+    return (
+      <Loader className="ui active huge inline centered">Loading...</Loader>
+    );
   } else {
-    return (deals.map(deal => {
-        return (
-            <Card key={deal.gameID} className="ui center aligned">
-              <Card.Content>                 
-                <div className="right floated meta">
-                  -{parseInt(deal.savings, 10)}%
-                </div>
-                  <i className="left floated star icon"></i>
-                {<img 
-                    src={deal.thumb} 
-                    alt={deal.title + " Thumbnail"} 
-                    className="ui image"
-                  />
-                  }                   
-                  <Card.Header id="card-header" className="ui header">
-                    {deal.title}
-                  </Card.Header>
-                <p>
-                  Metacritic Score: {
-                    <a href={"https://metacritic.com" + deal.metacriticLink}>
-                      {deal.metacriticScore}%
-                    </a>
-                    }
-                </p>
-                <p>
-                  Steam Rating: {
-                  <a href={"https://steamcommunity.com/app/" + deal.steamAppID + "/reviews/?browsefilter=toprated"}>
-                    {deal.steamRatingPercent}%
-                  </a>}  
-                </p>
-                <p>
-                  {deal.steamRatingText}
-                </p>
-                <p id="sale-price-text">
-                  <span id="normal-price-text">${deal.normalPrice}</span>
-                  &nbsp;| <strong>${deal.salePrice}</strong>
-                </p>
-                <Button className="ui icon button black">
-                  <i className="steam icon"></i>
-                </Button>              
-              </Card.Content>
-            </Card>
-        )
-      }))
+    return deals.map((deal) => {
+      return (
+        <Card key={deal.gameID} className="ui center aligned">
+          <Card.Content>
+            <div className="right floated meta">
+              -{parseInt(deal.savings, 10)}%
+            </div>
+            <i className="left floated star icon"></i>
+            <img
+              src={deal.thumb}
+              alt={deal.title + " Thumbnail"}
+              className="ui image"
+            />
+            <Card.Header id="card-header" className="ui header">
+              {deal.title}
+            </Card.Header>
+            <p>
+              Metacritic Score:{" "}
+              {
+                <a href={"https://metacritic.com" + deal.metacriticLink}>
+                  {deal.metacriticScore}%
+                </a>
+              }
+            </p>
+            <p>
+              Steam Rating:{" "}
+              {
+                <a
+                  href={
+                    "https://steamcommunity.com/app/" +
+                    deal.steamAppID +
+                    "/reviews/?browsefilter=toprated"
+                  }
+                >
+                  {deal.steamRatingPercent}%
+                </a>
+              }
+            </p>
+            <p>{deal.steamRatingText}</p>
+            <p id="sale-price-text">
+              <span id="normal-price-text">${deal.normalPrice}</span>
+              &nbsp;| <strong>${deal.salePrice}</strong>
+            </p>
+            <a
+              className="ui icon button black"
+              href={"https://www.cheapshark.com/redirect?dealID=" + deal.dealID}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="steam icon"></i>
+              <i className="top right corner tiny fitted external alternate icon"></i>
+            </a>
+          </Card.Content>
+        </Card>
+      );
+    });
   }
 }
