c845cccb9861dc8f36a270e8fe46e1fe9dd43bc8
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-07-02 07:14:19 -0700

    reformats fetch to improve component flexibility

--- src/components/DealsContainer.jsx
+++ src/components/DealsContainer.jsx
@@ -4,27 +4,24 @@ import "../styles/Deals.css";
 import DealCard from "./DealCard";
 import Header from "./Header";
 
-export default function Deals() {
+export default function Deals({ link }) {
   const [deals, setDeals] = useState(null);
 
-  async function fetchDeals() {
-    try {
-      const response = await fetch(
-        "https://www.cheapshark.com/api/1.0/deals?storeID=1&onSale=1&sortBy=Deal Rating&pageSize=12"
-      );
-      if (!response.ok) {
-        throw new Error("Request failed, Status: " + response.status);
-      }
-      const data = await response.json();
-      setDeals(data);
-    } catch (error) {
-      console.log("Error:", error);
-    }
-  }
-
   useEffect(() => {
+    async function fetchDeals() {
+      try {
+        const response = await fetch(link);
+        if (!response.ok) {
+          throw new Error("Request failed, Status: " + response.status);
+        }
+        const data = await response.json();
+        setDeals(data);
+      } catch (error) {
+        console.log("Error:", error);
+      }
+    }
     fetchDeals();
-  }, []);
+  }, [link]);
 
   return (
     <>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,11 @@ import Deals from "./DealsContainer";
 export default function SavedDeals() {
   return (
     <main>
-      <Deals />
+      <Deals
+        link={
+          "https://www.cheapshark.com/api/1.0/deals?storeID=1&onSale=1&sortBy=Deal Rating&pageSize=12"
+        }
+      />
     </main>
   );
 }
